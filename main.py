c636177059c6d452ee87ea6cb0cd7d0780c4eaca
Luis Bessa <spooooks72@gmail.com>
2024-05-14 14:21:34 -0300

    Add: fps certinho

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ import pygame
 tela = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Pega Doces")
 
+#CONFIGURAÇÃO PARA RODAR
+clock = pygame.time.Clock()
 
 rodando = True
+
+#DENTRO DO JOGO E COLOCANDO DENTRO DELE LA ELE 1000x
 while rodando == True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+
+
+
+    #FPS
+    clock.tick(60)
