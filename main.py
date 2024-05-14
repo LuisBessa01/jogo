4e82dc96bf9349985260fe32c02695cbf7494ac7
Luis Bessa <spooooks72@gmail.com>
2024-05-14 14:16:27 -0300

    Add: abrir e fechar tela

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 ##### Luis, o Fajuto
 
 
+import pygame
+#CRIANDO A TELA
+tela = pygame.display.set_mode((800, 500))
+pygame.display.set_caption("Pega Doces")
+
+
+rodando = True
+while rodando == True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
